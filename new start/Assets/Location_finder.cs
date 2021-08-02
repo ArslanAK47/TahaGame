5987504ab3b60e685dbcb22958349887612f89fe
ArslanAK47 <60480680+ArslanAK47@users.noreply.github.com>
2021-08-02 09:15:33 -0700

    PUSHING 2

--- new start/Assets/Location_finder.cs
+++ new start/Assets/Location_finder.cs
@@ -8,6 +8,8 @@ public class Location_finder : MonoBehaviour
 {
     public Text gpsOut;
     public bool isUpdating;
+    public double latitude;
+    public double longitude;
 
 
     private void Update()
@@ -60,7 +62,8 @@ public class Location_finder : MonoBehaviour
             gpsOut.text = "Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + 100f + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp;
             // Access granted and location value could be retrieved
             print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
-
+            latitude = Input.location.lastData.latitude;
+            longitude = Input.location.lastData.longitude;
         }
 
         // Stop service if there is no need to query location updates continuously
--- new start/Assets/test.cs
+++ new start/Assets/test.cs
@@ -6,20 +6,25 @@ public class test : MonoBehaviour
 {
     public string TextValue;
     public Text TextElement;
+    public double longitude1;
+    public double latitude1;
+    Location_finder location_finder;
 
-    public void get_location(double loci) {
-        TextElement.text = loci.ToString();
+    public void get_location(double loci, double loci2) {
+        TextElement.text = (loci.ToString() + " , " + loci2.ToString());
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        location_finder = FindObjectOfType<Location_finder>();
+        longitude1 = location_finder.longitude;
+        latitude1 = location_finder.latitude;
     }
 
     // Update is called once per frame
     void Update()
     {
-        get_location(23.12);
+        get_location(longitude1,latitude1);
     }
 }
